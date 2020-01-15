1d20979115a565545b4e5658b2461cc1f72c85ba
Shevi <shevids@gmail.com>
2020-01-16 08:36:50 +1100

    FIxed error with double and int types

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import "package:intl/intl.dart";
 
 import "../models/transaction.dart";
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -11,13 +12,13 @@ class Chart extends StatelessWidget {
   List<Map<String, Object>> get groupedTransactionList {
     return List.generate(7, (index) {
       final weekday = DateTime.now().subtract(Duration(days: index));
-      var totalSum = 0;
+      var totalSum = 0.0;
 
       for (var i = 0; i < recentTransactions.length; i++) {
         if (recentTransactions[i].date.day == weekday.day &&
             recentTransactions[i].date.month == weekday.month &&
             recentTransactions[i].date.year == weekday.year) {
-          totalSum += recentTransactions[i].amount.toInt();
+          totalSum += recentTransactions[i].amount;
         }
       }
 
@@ -25,6 +26,12 @@ class Chart extends StatelessWidget {
         "day": DateFormat.E().format(weekday).substring(0, 1),
         "amount": totalSum,
       };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionList.fold(0.0, (sum, item) {
+      return sum + item['amount'];
     });
   }
 
@@ -36,7 +43,11 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Row(
         children: groupedTransactionList.map((data) {
-          return Text('${data["day"]} : ${data["amount"].toString()}');
+          return ChartBar(
+            data['day'],
+            data['amount'],
+            (data['amount'] as double) / totalSpending,
+          );
         }).toList(),
       ),
     );
